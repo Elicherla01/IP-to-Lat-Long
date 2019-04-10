4af2643fe8db096310074fbebdc650e5ca2cb549
Ravindra Prasad <ravindraprasad@yahoo.com>
2019-04-10 11:43:10 +0530

    Initial file
    initial file

---
+++ fetch_lat_long.py
@@ -0,0 +1,19 @@
+import pandas as pd
+def fetch_lat_long(IP_Address):
+    #for i in list:
+    url = 'http://ip-api.com/json/' + IP_Address
+    req = urllib.request.Request(url)
+    out = urllib.request.urlopen(req).read()
+    #print(out)
+    a= out
+    k=ast.literal_eval(a.decode('utf-8'))
+    #print(k)
+    lat = k.get("lat")
+    lon = k.get("lon")
+    ll=[lat]
+    lo=[lon]
+    latlong=[ll,lo]
+    print(latlong)
+
+
+fetch_lat_long('203.191.56.3')
